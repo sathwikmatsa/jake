23f1ecd5ce71e3c0bb7a9e1c5b357dd4609e8aa8
Sathwik Matsa <sathwikmatsa@gmail.com>
2018-11-12 19:34:09 +0530

    modify elements extraction method to use css selector instead of xpath

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -2,7 +2,6 @@ var inputText = window.getSelection().toString();
 var url = window.location.href;
 var element = window.getSelection().anchorNode.parentNode;
 
-
 var so_url_template = "https://stackoverflow.com/search?q=";
 
 function getData(url, callback) {
@@ -15,32 +14,29 @@ function getData(url, callback) {
 }
 
 // this function converts the Xpath object into a list and returns it
-function xpath(xpath_string, dom) {
-  var result = [];
-  var nodes_snapshot = document.evaluate(xpath_string, dom, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-  // document.evaluate is used to select the elements based on xpath
-  // note that the second argument to evaluate is <dom>, which essentially indicates that xpath selection is to be performed within <dom> and not the current document.
-  for (var i = 0; i < nodes_snapshot.snapshotLength; i++) {
-      result.push(nodes_snapshot.snapshotItem(i));
+function css_selector(CSS_selector_string1, CSS_selector_string2, dom) {
+  var data = [];
+  var nodeList = dom.querySelectorAll(CSS_selector_string1);
+  for(var i = 0; i<nodeList.length; i++){
+    var element = nodeList[i].querySelector(CSS_selector_string2);
+    if(element) data.push(element);
   }
-  return result;
+  return data;
 }
 
 function createElementFromHTML(htmlString) {
   var div = document.createElement('div');
   div.innerHTML = htmlString.trim();
-  return div.firstChild;
+  return div;
 }
 
 function filterSO(data, node) {
   var dom = createElementFromHTML(data);
-
-  var data = xpath("//div[contains(@class,'summary')]/div/h3", dom);
+  var data = css_selector(".question-summary", ".question-hyperlink", dom);
   display(data, node);
 }
 
 function searchStackOverflow(search_text, node) {
-
   var so_url = so_url_template + search_text;
 
   getData(so_url, function(data) {
@@ -59,13 +55,13 @@ function display(data, node) {
       var row = document.createElement("tr");
       var cell = document.createElement('td');
       var example = document.createElement('a');
-      example.href = "https://stackoverflow.com"+data[i].getElementsByClassName("question-hyperlink")[0].getAttribute("href");
+      example.href = "https://stackoverflow.com"+data[i].getAttribute("href");
       example.textContent = data[i].innerText
       cell.appendChild(example);
       row.appendChild(cell);
       tableBody.appendChild(row);
     }
-    heading.innerHTML = "<strong>Code Examples</strong>: (" + data.slice(1,10).length.toString() + " found)";
+    heading.innerHTML = "<strong>Code Examples</strong>: (" + data.slice(0,10).length.toString() + " found)";
     table.appendChild(tableBody);
     container.appendChild(heading);
     container.appendChild(table);
@@ -75,6 +71,5 @@ function display(data, node) {
     container.style.color = '#333';
     node.parentNode.insertBefore(container, node.nextSibling);
 }
-
 searchStackOverflow(inputText, element);
 alert("Success!");
